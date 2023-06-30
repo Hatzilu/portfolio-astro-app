3ed4efcf091876b2af89eb126605586fc7957b75
Kapeet <itamar246@gmail.com>
2023-06-30 17:55:27 +0300

    oopsie 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://kapeet.github.io',
+	base: '/portfolio-astro-app',
 	integrations: [tailwind(), react()],
 });
